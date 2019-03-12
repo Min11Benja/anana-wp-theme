d0eb9f540c15f452f2d5aff91d9bbab13e9c3f29
min11benja <redoxz811@gmail.com>
2019-03-12 16:26:57 -0600

    added service page

--- servicio-page.php
+++ servicio-page.php
@@ -15,7 +15,8 @@ Template Name: Servicio
 
 get_header();
 ?>
-    <main id="general_page">
+
+<main id="general_page">
 
 <section id="servicios-hero">
  
@@ -36,131 +37,129 @@ get_header();
 
 </section>
 <!-- /section -->
+
 <section id="service-desc">
-            <div class="container-fluid ">
+	<div class="container-fluid ">
 
-                <!--ROW 1 -->
-                <div class="row ">
+		<!--ROW 1 -->
+		<div class="row ">
 
-                    <div class="col-lg-6 col-md-6 col-sm-12">
-
-
-                        <div class="d-flex flex-row bd-highlight mb-3">
-                            <div class="p-2 bd-highlight">
-                                <h3>GESTIÓN DE <br>REDES SOCIALES</h3>
-                            </div>
-                            <div class="p-2 bd-highlight"><img src="<?php bloginfo('template_directory');?>/assets/img/redes-icon-01.png" alt="" class="img-responsive" style="margin: 0 auto;"></div>
-
-                        </div>
-
-                        <p class="text-dark">
-                            Te ayudamos a crea una comunidad de usuarios identificados con tu marca y convertir a tus visitas en amigos, a los amigos en clientes y a los clientes en embajadores de tu marca.
-
-                        </p>
-
-                    </div>
-
-                    <div class="col-lg-6 col-md-6 col-sm-12">
-
-                        <div class="d-flex flex-row bd-highlight mb-3">
-                            <div class="p-2 bd-highlight">
-                                <h3>MARKETING <br>360º</h3>
-                            </div>
-                            <div class="p-2 bd-highlight"><img src="<?php bloginfo('template_directory');?>/assets/img/redes-360-icon-01.png" alt="" class="img-responsive" style="margin: 0 auto;"></div>
-
-                        </div>
-
-                        <p class="text-dark">
-                            En marketing 360º nos dirigimos a todos los públicos que estén relacionados con tu marca y nos enfocamos en darles a conocer la experiencia de colaborar contigo o de consumir tus productos a través de diversos canales y estrategias.
-                        </p>
-                    </div>
-
-                </div>
+			<div class="col-lg-6 col-md-6 col-sm-12">
 
 
-                <!--ROW 2 -->
-                <div class="row ">
+				<div class="d-flex flex-row bd-highlight mb-3">
+					<div class="p-2 bd-highlight">
+						<h3>GESTIÓN DE <br>REDES SOCIALES</h3>
+					</div>
+					<div class="p-2 bd-highlight"><img src="<?php bloginfo('template_directory');?>/assets/img/redes-icon-01.png" alt="" class="img-responsive" style="margin: 0 auto;"></div>
 
-                    <div class="col-lg-6 col-md-6 col-sm-12">
+				</div>
 
+				<p class="text-dark">
+					Te ayudamos a crea una comunidad de usuarios identificados con tu marca y convertir a tus visitas en amigos, a los amigos en clientes y a los clientes en embajadores de tu marca.
 
-                        <div class="d-flex flex-row bd-highlight mb-3">
-                            <div class="p-2 bd-highlight">
-                                <h3>POSICIONAMIENTO <br>DE MARCA</h3>
-                            </div>
-                            <div class="p-2 bd-highlight"><img src="<?php bloginfo('template_directory');?>/assets/img/posicionamiento-01.png" alt="" class="img-responsive" style="margin: 0 auto;"></div>
+				</p>
 
-                        </div>
+			</div>
 
-                        <p class="text-dark">
-                           Cada minuto hay miles de posibles clientes buscando un servicio como el tuyo, y si no te encuentran a ti se van a ir con alguien más. Que tu negocio aparezca en uno de los buscadores más reconocidos mundialmente es fundamental hoy en día.
-                        </p>
+			<div class="col-lg-6 col-md-6 col-sm-12">
 
-                    </div>
+				<div class="d-flex flex-row bd-highlight mb-3">
+					<div class="p-2 bd-highlight">
+						<h3>MARKETING <br>360º</h3>
+					</div>
+					<div class="p-2 bd-highlight"><img src="<?php bloginfo('template_directory');?>/assets/img/redes-360-icon-01.png" alt="" class="img-responsive" style="margin: 0 auto;"></div>
 
-                    <div class="col-lg-6 col-md-6 col-sm-12">
+				</div>
 
-                        <div class="d-flex flex-row bd-highlight mb-3">
-                            <div class="p-2 bd-highlight">
-                                <h3>ELABORACIÓN DE <br>PAGINAS WEB</h3>
-                            </div>
-                            <div class="p-2 bd-highlight"><img src="<?php bloginfo('template_directory');?>/assets/img/web-icon-01.png" alt="" class="img-responsive" style="margin: 0 auto;"></div>
+				<p class="text-dark">
+					En marketing 360º nos dirigimos a todos los públicos que estén relacionados con tu marca y nos enfocamos en darles a conocer la experiencia de colaborar contigo o de consumir tus productos a través de diversos canales y estrategias.
+				</p>
+			</div>
 
-                        </div>
-
-                        <p class="text-dark">
-                            Haz de tu página web una experiencia para tu usuario y así́ que la misma funcione como un generador de prospectos y una extensión de tu negocio.
-                        </p>
-                    </div>
-
-                </div>
-
-                <!--ROW 3 -->
-                <div class="row ">
-                    
-                       <div class="col-lg-6 col-md-6 col-sm-12">
-
-                        <div class="d-flex flex-row bd-highlight mb-3">
-                            <div class="p-2 bd-highlight">
-                                <h3>IDENTIDAD <br>CORPORATIVA</h3>
-                            </div>
-                            <div class="p-2 bd-highlight"><img src="<?php bloginfo('template_directory');?>/assets/img/diseno-icon-01.png" alt="" class="img-responsive" style="margin: 0 auto;"></div>
-
-                        </div>
-
-                        <p class="text-dark">
-                            Contar con una identidad corporativa te permite: Ser reconocida fácilmente por tus clientes y sobretodo, ser recordado y tomado con seriedad.
-
-                        </p>
-                    </div>
-
-                    <div class="col-lg-6 col-md-6 col-sm-12">
+		</div>
 
 
-                        <div class="d-flex flex-row bd-highlight mb-3">
-                            <div class="p-2 bd-highlight">
-                                <h3>INBOUND <br>MARKETING</h3>
-                            </div>
-                            <div class="p-2 bd-highlight"><img src="<?php bloginfo('template_directory');?>/assets/img/inbound-icon-01.png" alt="" class="img-responsive" style="margin: 0 auto;"></div>
+		<!--ROW 2 -->
+		<div class="row ">
 
-                        </div>
-
-                        <p class="text-dark">
-                            El inbound marketing es una metodología que combina técnicas de marketing y publicidad no intrusivas con la finalidad de contactar con un usuario al principio de su proceso de compra y acompañarle hasta la transacción final.
-                        </p>
-
-                    </div>
-
-                 
-
-                </div>
+			<div class="col-lg-6 col-md-6 col-sm-12">
 
 
-            </div>
-        </section>
-        <!-- /section -->
+				<div class="d-flex flex-row bd-highlight mb-3">
+					<div class="p-2 bd-highlight">
+						<h3>POSICIONAMIENTO <br>DE MARCA</h3>
+					</div>
+					<div class="p-2 bd-highlight"><img src="<?php bloginfo('template_directory');?>/assets/img/posicionamiento-01.png" alt="" class="img-responsive" style="margin: 0 auto;"></div>
 
-    </main>
+				</div>
+
+				<p class="text-dark">
+				   Cada minuto hay miles de posibles clientes buscando un servicio como el tuyo, y si no te encuentran a ti se van a ir con alguien más. Que tu negocio aparezca en uno de los buscadores más reconocidos mundialmente es fundamental hoy en día.
+				</p>
+
+			</div>
+
+			<div class="col-lg-6 col-md-6 col-sm-12">
+
+				<div class="d-flex flex-row bd-highlight mb-3">
+					<div class="p-2 bd-highlight">
+						<h3>ELABORACIÓN DE <br>PAGINAS WEB</h3>
+					</div>
+					<div class="p-2 bd-highlight"><img src="<?php bloginfo('template_directory');?>/assets/img/web-icon-01.png" alt="" class="img-responsive" style="margin: 0 auto;"></div>
+
+				</div>
+
+				<p class="text-dark">
+					Haz de tu página web una experiencia para tu usuario y así́ que la misma funcione como un generador de prospectos y una extensión de tu negocio.
+				</p>
+			</div>
+
+		</div>
+
+		<!--ROW 3 -->
+		<div class="row ">
+			
+			   <div class="col-lg-6 col-md-6 col-sm-12">
+
+				<div class="d-flex flex-row bd-highlight mb-3">
+					<div class="p-2 bd-highlight">
+						<h3>IDENTIDAD <br>CORPORATIVA</h3>
+					</div>
+					<div class="p-2 bd-highlight"><img src="<?php bloginfo('template_directory');?>/assets/img/diseno-icon-01.png" alt="" class="img-responsive" style="margin: 0 auto;"></div>
+
+				</div>
+
+				<p class="text-dark">
+					Contar con una identidad corporativa te permite: Ser reconocida fácilmente por tus clientes y sobretodo, ser recordado y tomado con seriedad.
+
+				</p>
+			</div>
+
+			<div class="col-lg-6 col-md-6 col-sm-12">
+
+				<div class="d-flex flex-row bd-highlight mb-3">
+					<div class="p-2 bd-highlight">
+						<h3>INBOUND <br>MARKETING</h3>
+					</div>
+					<div class="p-2 bd-highlight"><img src="<?php bloginfo('template_directory');?>/assets/img/inbound-icon-01.png" alt="" class="img-responsive" style="margin: 0 auto;"></div>
+
+				</div>
+
+				<p class="text-dark">
+					El inbound marketing es una metodología que combina técnicas de marketing y publicidad no intrusivas con la finalidad de contactar con un usuario al principio de su proceso de compra y acompañarle hasta la transacción final.
+				</p>
+
+			</div>
+
+		</div>
+
+	</div>
+</section>
+<!-- /section -->
+
+</main>
+
 <?php
 
 get_footer();
