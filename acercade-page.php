2d335ca4f1ac959f3058988329d1823cf3474c62
min11benja <redoxz811@gmail.com>
2019-03-12 15:16:32 -0600

    added the about page html and edited dource links

--- acercade-page.php
+++ acercade-page.php
@@ -16,26 +16,129 @@ Template Name: Acerca de
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<main id="general_page">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+        <div class="container_styled_1">
 
-			get_template_part( 'template-parts/content', 'page' );
+            <div class="container margin_60_35">
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+                <div class="row">
 
-		endwhile; // End of the loop.
-		?>
+                    <div class="col-lg-12">
+                         <h1 class="text-dark text-center">ACERCA DE <strong>NOSOTROS</strong></h1><br>
+                        
+                        <p class="text-dark">Somos la agencia externa de marketing que quiere ayudarte a encontrar la esencia y valor de tu negocio para comunicarlo por medio de experiencias y comunicación estratégica.</p>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+                    </div>
+
+
+                </div>
+                <!-- End row -->
+            </div>
+
+        </div>
+
+        <section id="about-mision">
+           
+
+            <div class="container margin_60_35">
+
+                <div class="row">
+
+                    <div class="col-lg-12">
+                     
+
+                        <h2 class="nomargin_top text-yellow">MISIÓN</h2>
+
+                        <p class="lead text-white">Comunicar con estrategia y creatividad la esencia de las marcas que tienen sentido de innovación y con gusto por hacer las cosas diferentes al resto, hacerlo siempre con calidad y enfocándonos en aportar experiencias al cliente final.</p>
+                        
+                        
+                          <h2 class="nomargin_top text-yellow">VISIÓN</h2>
+
+                        <p class="lead text-white ">Creemos en el cambio, pues lo vemos siempre como una oportunidad para evolucionar e impactar en las marcas para que estas aporten valor al cliente final.</p>
+
+                        <p class="lead text-white ">Hacemos lo que hacemos porque nos inspira hacer las cosas diferentes, brindar valor a la comunidad contribuyendo con estrategias de comunicación que se transforman en experiencias. </p>
+
+                    </div>
+
+
+                </div>
+                <!-- End row -->
+            </div>
+
+            
+        </section>
+               
+
+        
+        
+        <section id="about-service">
+
+            <div class="container_styled_1">
+
+                <div class="container margin_60_35">
+                    
+                    <div class="row">
+                        <div class="col-12">
+                         <div class="main_title">
+                                <h2><em></em>¿QUÉ PODEMOS HACER POR TI?</h2>
+
+                            </div>
+                        </div>
+                    
+                    </div>
+
+                    <div class="row">
+
+                        
+
+                        <div class="col-lg-6">
+
+
+                           
+
+                            <ul id="about-service-list" class="list-unstyled footer-list-item pt10">
+                                <li>
+                                    Manejo y promoción de <strong>redes sociales</strong>.
+                                </li>
+                                <li>
+                                    Desarrollo de <strong>contenidos</strong>.
+                                </li>
+
+                                <li>
+                                    Producción <strong>audiovisual</strong>.
+                                </li>
+
+                                <li>
+                                    <strong>Inbound</strong> Marketing.
+                                </li>
+
+                                <li>
+                                    Branding.
+                                </li>
+                                <li>
+                                    Relaciones Públicas <strong>360</strong>.
+                                </li>
+
+
+                            </ul>
+
+                        </div>
+                        <div class="col-lg-5 ml-lg-5 add_top_30 hidden-md-down">
+
+                            <img src="<?php bloginfo('template_directory');?>/assets/img/half-logo-bg-01.png" alt="" class="img-fluid">
+
+                        </div>
+
+                    </div>
+                    <!-- End row -->
+                </div>
+
+            </div>
+        </section>
+
+    </main>
 
 <?php
-get_sidebar();
+
 get_footer();
