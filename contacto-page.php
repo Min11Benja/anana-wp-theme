e266e9a5e2c03ee660b119e55d942156c082e209
min11benja <redoxz811@gmail.com>
2019-03-12 12:06:33 -0600

    added the Template name to each page

--- contacto-page.php
+++ contacto-page.php
@@ -1,7 +1,9 @@
 <?php
+/*
+Template Name: Contacto
+*/
 /**
- * The template for displaying all pages
- *
+
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
  * and that other 'pages' on your WordPress site may use a
--- inicio-page.php
+++ inicio-page.php
@@ -1,11 +1,8 @@
 <?php
+/*
+Template Name: Inicio
+*/
 /**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
@@ -36,5 +33,5 @@ get_header();
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
+
 get_footer();
