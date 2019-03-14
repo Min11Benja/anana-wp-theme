0f4eea07feb16091ada7edb43eea0aefb3c4c875
min11benja <redoxz811@gmail.com>
2019-03-14 12:48:44 -0600

    added the audiovisual page

---
+++ audiovisual-page.php
@@ -0,0 +1,163 @@
+<?php
+/*
+Template Name: Audiovisual
+*/
+/**
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package anana-mkt
+ */
+
+get_header();
+?>
+
+<div class="nk-gap-5"></div>
+    <div class="container">
+        <div class="row">
+
+            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 pb-20">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Nh_9AVGcPO8" allowfullscreen></iframe>
+                </div>
+
+
+            </div>
+
+
+            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 pb-20">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/sYiV4MUNxeU" allowfullscreen></iframe>
+                </div>
+
+            </div>
+
+            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 pb-20">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/cU_wPslPQcw" allowfullscreen></iframe>
+                </div>
+
+            </div>
+
+            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 pb-20">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/z8C-9CEJR3E" allowfullscreen></iframe>
+                </div>
+
+            </div>
+
+            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 pb-20">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/KNst8N-LLy0" allowfullscreen></iframe>
+                </div>
+
+            </div>
+
+            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 pb-20">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/p-K481VofQU" allowfullscreen></iframe>
+                </div>
+
+            </div>
+
+            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 pb-20">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/TUjZO6gY4kc" allowfullscreen></iframe>
+                </div>
+
+            </div>
+
+            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 pb-20">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/COXtuHAczoE" allowfullscreen></iframe>
+                </div>
+
+            </div>
+
+            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 pb-20">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/k5rSmI9Phbk" allowfullscreen></iframe>
+                </div>
+
+            </div>
+
+            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 pb-20">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/-Y1Zem8Ek6w" allowfullscreen></iframe>
+                </div>
+
+            </div>
+
+            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 pb-20">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/7MBvwZbI7ro" allowfullscreen></iframe>
+                </div>
+
+            </div>
+
+            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 pb-20">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/rNeOq96rkSs" allowfullscreen></iframe>
+                </div>
+
+            </div>
+
+            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 pb-20">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/2PP-X2P1HAA" allowfullscreen></iframe>
+                </div>
+
+
+            </div>
+
+            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 pb-20">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/ufbKaae-eZg" allowfullscreen></iframe>
+                </div>
+
+
+            </div>
+           
+            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 pb-20">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/DUpwRUSnGy8" allowfullscreen></iframe>
+                </div>
+
+            </div>
+            
+                  <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 pb-20">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/uRip6iCo1es" allowfullscreen></iframe>
+                </div>
+
+            </div>
+            
+              <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 pb-20">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/a8-nXrWIoMc" allowfullscreen></iframe>
+                </div>
+
+            </div>
+            
+              <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 pb-20">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/wgutH4TRyEc" allowfullscreen></iframe>
+                </div>
+
+            </div>
+
+            <div class="nk-gap-5"></div>
+            <div class="w-100"></div>
+
+            <div class="nk-gap-5"></div>
+
+        </div>
+    </div>
+
+<?php
+
+get_footer();
