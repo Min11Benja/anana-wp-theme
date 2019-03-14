d4712f5f27bcf6e06316d3b1cf97282f50ebc5fa
min11benja <redoxz811@gmail.com>
2019-03-14 11:35:56 -0600

    added portfilio asg tek and the get page link function for specific web link tags

--- portafolio-asg-page.php
+++ portafolio-asg-page.php
@@ -16,26 +16,323 @@ Template Name: Port-Asg
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<div id="asgtek-hero" class="container_styled_1">
+        <div class="container margin_60_35">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="text-dark text-center">DISEÑO GRÁFICO Y WEB<br>
+                        <strong>ASG Tek</strong> ERP SOLUTIONS</h1><br>
+                </div>
+            </div>
+            <!-- End row -->
+        </div>
+    </div>
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+    <div class="container">
+        <div class="nk-portfolio-single">
 
-			get_template_part( 'template-parts/content', 'page' );
+            <div class="nk-gap-4 mb-2"></div>
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+            <div class="row vertical-gap">
+                <div class="col-lg-8">
+                    <h3 class="nk-portfolio-title">Como ayudamos a ASG Tek con su marca</h3>
+                    <div class="nk-portfolio-info">
+                        <div class="nk-portfolio-text">
 
-		endwhile; // End of the loop.
-		?>
+                            <p class="text-dark">Empresa con presencia en USA y México distribuidores de sistemas de información y control empresarial conocido como Sistemas ERP, es una marca con carácter tecnológico y distinguido por su asesoría especializada.</p>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-lg-4">
+                    <table class="nk-portfolio-details">
+                        <tr>
+                            <td>
+                                <strong>Cliente:</strong>
+                            </td>
+                            <td>
+                                <a href="https://s3.amazonaws.com/anana-temp-site/asg-tek-temp-site/index.html" target="_blank">ASG Tek </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <strong>Fecha:</strong>
+                            </td>
+                            <td>15.06.2018</td>
+                        </tr>
+
+
+                        <tr>
+                            <td>
+                                <strong>
+                                    Servicio:
+                                </strong>
+                            </td>
+                            <td>
+                                Branding y estandarización de imagen<br>
+                                Desarrollo Web<br>
+                                Presencia en social media y optimización en campañas<br>
+                                Relaciones públicas<br>
+                                Kit de ventas
+                            </td>
+
+                        </tr>
+
+                        <tr>
+                            <td>
+                                <p class="text-dark"> <a href="https://www.facebook.com/asgtek" target="_blank"><i class="fa fa-facebook"></i></a>
+                                    <a href="https://www.linkedin.com/company/18749497" target="_blank"><i class="fa fa-linkedin"></i></a>
+
+                                </p>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            <div class="nk-gap-4 mnb-10"></div>
+
+        </div>
+    </div>
+
+
+    <!-- START: Features -->
+    <div class="nk-box bg-white">
+        <div class="nk-gap-5 mnt-6"></div>
+        <div class="container">
+
+            <div class="row vertical-gap md-gap">
+
+                <div class="col-md-6 col-lg-3">
+                    <div class="nk-ibox-3">
+
+                        <div class="nk-ibox-cont">
+                            <div class="nk-ibox-title">Branding</div>
+                            <div class="nk-ibox-text"></div>
+                        </div>
+                        <div class="nk-ibox-icon">
+                            <i class="fa fa-calendar" aria-hidden="true"></i>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3">
+                    <div class="nk-ibox-3">
+
+                        <div class="nk-ibox-cont">
+                            <div class="nk-ibox-title">Relaciones publicas</div>
+                            <div class="nk-ibox-text"></div>
+                        </div>
+                        <div class="nk-ibox-icon">
+                            <i class="fa fa-image"></i>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3">
+                    <div class="nk-ibox-3">
+
+                        <div class="nk-ibox-cont">
+                            <div class="nk-ibox-title">Redes Sociales</div>
+                            <div class="nk-ibox-text"></div>
+                        </div>
+                        <div class="nk-ibox-icon">
+                            <i class="fa fa-newspaper-o"></i>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3">
+                    <div class="nk-ibox-3">
+
+                        <div class="nk-ibox-cont">
+                            <div class="nk-ibox-title">Desarrollo Web</div>
+                            <div class="nk-ibox-text"></div>
+                        </div>
+                        <div class="nk-ibox-icon">
+                            <i class="fa fa-magic" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="nk-gap-5 mnt-6"></div>
+    </div>
+    <!-- END: Features -->
+
+
+    <div class="nk-gap-5"></div>
+
+    <div id="asgtek-pik1" class="container portfolio-grid-1">
+        <div class="nk-isotope nk-isotope-1-cols nk-isotope-gap nk-popup-gallery nk-portfolio-list">
+
+            <div class="nk-isotope-item" data-filter="Design">
+                <div class="nk-portfolio-item nk-portfolio-item-square nk-portfolio-item-info-style-1">
+
+                    <div class="nk-portfolio-item-image">
+                        <div style="background-image: url('<?php bloginfo('template_directory');?>/assets/img/asgtek/asgtek-1.png');"></div>
+                    </div>
+                    <div class="nk-portfolio-item-info nk-portfolio-item-info-center text-center bg-main-9">
+                        <div>
+                            <div class="portfolio-item-icon">
+                                <i class="fa fa-eye"></i>
+                                <span class="overlay-img-text"> Branding</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+    <div class="container portfolio-grid-1">
+        <div class="nk-isotope nk-isotope-1-cols nk-isotope-gap nk-popup-gallery nk-portfolio-list">
+
+            <div class="nk-isotope-item" data-filter="Design">
+                <div class="nk-portfolio-item nk-portfolio-item-square nk-portfolio-item-info-style-1">
+
+                    <div class="nk-portfolio-item-image">
+                        <div style="background-image: url('<?php bloginfo('template_directory');?>/assets/img/asgtek/asgtek-2.png');"></div>
+                    </div>
+                    <div class="nk-portfolio-item-info nk-portfolio-item-info-center text-center bg-main-9">
+                        <div>
+                            <div class="portfolio-item-icon">
+                                <i class="fa fa-eye"></i>
+                                <span class="overlay-img-text"> Presentación de ventas</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="container portfolio-grid-1">
+        <div class="nk-isotope nk-isotope-1-cols nk-isotope-gap nk-popup-gallery nk-portfolio-list">
+
+            <div class="nk-isotope-item" data-filter="Design">
+                <div class="nk-portfolio-item nk-portfolio-item-square nk-portfolio-item-info-style-1">
+
+                    <div class="nk-portfolio-item-image">
+                        <div style="background-image: url('<?php bloginfo('template_directory');?>/assets/img/asgtek/asgtek-3.png');"></div>
+                    </div>
+                    <div class="nk-portfolio-item-info nk-portfolio-item-info-center text-center bg-main-9">
+                        <div>
+                            <div class="portfolio-item-icon">
+                                <i class="fa fa-eye"></i>
+                                <span class="overlay-img-text"> Tríptico</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+    <div class="container portfolio-grid-1">
+        <div class="nk-isotope nk-isotope-1-cols nk-isotope-gap nk-popup-gallery nk-portfolio-list">
+
+            <div class="nk-isotope-item" data-filter="Design">
+                <div class="nk-portfolio-item nk-portfolio-item-square nk-portfolio-item-info-style-1">
+
+                    <div class="nk-portfolio-item-image">
+                        <div style="background-image: url('<?php bloginfo('template_directory');?>/assets/img/asgtek/asgtek-4.png');"></div>
+                    </div>
+                    <div class="nk-portfolio-item-info nk-portfolio-item-info-center text-center bg-main-9">
+                        <div>
+                            <div class="portfolio-item-icon">
+                                <i class="fa fa-eye"></i>
+                                <span class="overlay-img-text"> Redes Sociales</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div id="asgtek-pik-5" class="container portfolio-grid-1">
+        <div class="nk-isotope nk-isotope-1-cols nk-isotope-gap nk-popup-gallery nk-portfolio-list">
+
+            <div class="nk-isotope-item" data-filter="Design">
+                <div class="nk-portfolio-item nk-portfolio-item-square nk-portfolio-item-info-style-1">
+                    <a href="https://s3.amazonaws.com/anana-temp-site/asg-tek-temp-site/index.html" class="nk-portfolio-item-link nk-gallery-item" target="_blank"></a>
+                    <div class="nk-portfolio-item-image">
+                        <div style="background-image: url('<?php bloginfo('template_directory');?>/assets/img/asgtek/asgttek-5.png');"></div>
+                    </div>
+                    <div class="nk-portfolio-item-info nk-portfolio-item-info-center text-center bg-main-9">
+                        <div>
+                            <div class="portfolio-item-icon">
+                                <i class="fa fa-link"></i>
+                                <span class="overlay-img-text"> Pagina Web</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="nk-gap-5"></div>
+
+    <div class="nk-gap-5"></div>
+
+    <div class="d-flex justify-content-center">
+        <a href="https://s3.amazonaws.com/anana-temp-site/asg-tek-temp-site/index.html" class="btn btn-outline-secondary mb5 btn-rounded" target="_blank">VISITAR SITIO WEB</a>
+    </div>
+
+    <div class="nk-gap-5"></div>
+
+    <div class="nk-gap-5"></div>
+
+
+    <!-- START: Need a Project? -->
+    <div class="bg-black text-center">
+        <div class="nk-gap-5 mnb-12"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 offset-lg-2">
+                    <h2 class="text-white">¿Quieres algo similar?</h2>
+                    <div class="nk-gap-1 mnt-15"></div>
+                    <p class="text-white">Cuéntanos lo que estás buscando hacer o promover. Le damos un vistazo y vemos si esto podría ser el comienzo de algo hermoso para ti y tu negocio.</p>
+                    <div class="nk-gap mnt-9"></div>
+					<!--contact page link id=28 -->
+                    <a href="<?php echo get_page_link(28); ?>" class="btn btn-outline-black btn-outline-secondary mb5 btn-rounded">CONTÁCTANOS</a>
+                </div>
+            </div>
+        </div>
+        <div class="nk-gap-5"></div>
+    </div>
+    <!-- END: Need a Project? -->
+
+
+    <div class="nk-gap-5"></div>
+
+    <!-- START: Pagination -->
+    <div class="nk-pagination nk-pagination-center">
+        <div class="container">
+            <a class="nk-pagination-prev" href="#">
+            </a>
+
+			<!--portfolio cat page link id=20 -->
+            <a class="nk-pagination-center" href="<?php echo get_page_link(20); ?>">
+                <span class="nk-icon-squares"></span>
+            </a>
+			<!--yeiyari portfolio page link id=74 -->
+            <a class="nk-pagination-next" href="<?php echo get_page_link(74); ?>">Proyecto siguiente <span class="fa fa-angle-right"></span> </a>
+        </div>
+    </div>
+    <!-- END: Pagination -->
 
 <?php
-get_sidebar();
+
 get_footer();
