a82c7834b7b08a91da7ba212a90de0492dd72ea7
min11benja <redoxz811@gmail.com>
2019-03-14 10:50:30 -0600

    added the portfolio cat page

--- portafolio-cat-page.php
+++ portafolio-cat-page.php
@@ -16,26 +16,341 @@ Template Name: Port-Cat
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<main id="general_page">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+<div class="container">
 
-			get_template_part( 'template-parts/content', 'page' );
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+	<div class="nk-portfolio-list nk-isotope nk-isotope-3-cols nk-isotope-gap">
 
-		endwhile; // End of the loop.
-		?>
+		<div class="nk-isotope-item" data-filter="Social">
+			<div class="nk-portfolio-item nk-portfolio-item-info-style-2 nk-portfolio-item-sm">
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+				<div class="nk-portfolio-item-image">
+					<div style="background-image: url('<?php bloginfo('template_directory');?>/assets/img/logos/MARCAS-LOGOS800X800_RICHARDS.jpg')"></div>
+				</div>
+				<div class="nk-portfolio-item-info nk-portfolio-item-info-center text-center">
+
+					<div>
+						<h2 class="portfolio-item-title h3">Richards</h2>
+
+						<div class="portfolio-item-category">
+
+							<a href="http://www.richards.com.mx/" class="nk-portfolio-item-link" target="_blank">
+								<i class="fa fa-globe pdleft-2-4"></i>
+							</a>
+
+
+							<a href="https://www.facebook.com/RichardsSLP/" class="nk-portfolio-item-link" target="_blank">
+								<i class="fa fa-facebook pdleft-3-4"></i>
+							</a>
+
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="nk-isotope-item" data-filter="Social">
+			<div class="nk-portfolio-item nk-portfolio-item-info-style-2 nk-portfolio-item-sm">
+
+				<div class="nk-portfolio-item-image">
+					<div style="background-image: url('<?php bloginfo('template_directory');?>/assets/img/logos/MARCAS-LOGOS800X800_LOVELEAF.jpg')"></div>
+				</div>
+				<div class="nk-portfolio-item-info nk-portfolio-item-info-center text-center">
+					<div>
+						<h2 class="portfolio-item-title h3">Love Leaf</h2>
+						<div class="portfolio-item-category">
+
+							<a href="https://www.instagram.com/love_leaf_vegan/" class="nk-portfolio-item-link" target="_blank">
+								<i class="fa fa-instagram pdleft-2-4"></i>
+							</a>
+
+							<a href="https://www.facebook.com/loveleafvegandelight/" class="nk-portfolio-item-link" target="_blank">
+								<i class="fa fa-facebook pdleft-3-4"></i>
+							</a>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="nk-isotope-item" data-filter="Social">
+			<div class="nk-portfolio-item nk-portfolio-item-info-style-2 nk-portfolio-item-sm">
+
+				<div class="nk-portfolio-item-image">
+					<div style="background-image: url('<?php bloginfo('template_directory');?>/assets/img/logos/MARCAS-LOGOS800X800_AZULMARINO.jpg')"></div>
+				</div>
+				<div class="nk-portfolio-item-info nk-portfolio-item-info-center text-center">
+					<div>
+						<h2 class="portfolio-item-title h3">Azul Marino</h2>
+						<div class="portfolio-item-category">
+
+							<a href="https://www.facebook.com/RestauranteAzulMarino/" class="nk-portfolio-item-link" target="_blank">
+								<i class="fa fa-facebook pdleft-2-4"></i>
+							</a>
+
+							<a href="https://www.instagram.com/azulmarino_restaurante/" class="nk-portfolio-item-link" target="_blank">
+								<i class="fa fa-instagram pdleft-3-4"></i>
+							</a>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="nk-isotope-item" data-filter="Web Design">
+			<div class="nk-portfolio-item nk-portfolio-item-info-style-2 nk-portfolio-item-sm">
+			  
+				<div class="nk-portfolio-item-image">
+					<div style="background-image: url('<?php bloginfo('template_directory');?>/assets/img/logos/MARCAS-LOGOS800X800_ASGTEK.jpg')"></div>
+				</div>
+				<div class="nk-portfolio-item-info nk-portfolio-item-info-center text-center">
+					<div>
+						<h2 class="portfolio-item-title h3">ASG Tek</h2>
+						<div class="portfolio-item-category">
+
+							<a href="https://www.facebook.com/asgtek" class="nk-portfolio-item-link" target="_blank">
+								<i class="fa fa-facebook pdleft-1-4"></i>
+							</a>
+
+							<a href="https://www.linkedin.com/company/18749497" class="nk-portfolio-item-link" target="_blank">
+								<i class="fa fa-linkedin pdleft-2-4"></i>
+							</a>
+							
+							 <a href="https://s3.amazonaws.com/anana-temp-site/asg-tek-temp-site/index.html" class="nk-portfolio-item-link" target="_blank">
+								<i class="fa fa-globe pdleft-3-4"></i>
+							</a>
+
+							<a href="port/portafolio-asg-tek.html" class="nk-portfolio-item-link">
+								<i class="fa fa-newspaper-o pdleft-4-4"></i>
+							</a>
+							
+						</div>
+
+
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="nk-isotope-item" data-filter="Web Landing">
+			<div class="nk-portfolio-item nk-portfolio-item-info-style-2 nk-portfolio-item-sm">
+				<a href="http://www.ilbasilico.mx/" class="nk-portfolio-item-link" target="_blank"></a>
+				<div class="nk-portfolio-item-image">
+					<div style="background-image: url('<?php bloginfo('template_directory');?>/assets/img/logos/MARCAS-LOGOS800X800_ILBASILICO.jpg')"></div>
+				</div>
+				<div class="nk-portfolio-item-info nk-portfolio-item-info-center text-center">
+					<div>
+						<h2 class="portfolio-item-title h3">IlBasilico</h2>
+							<div class="portfolio-item-category ">
+
+							<a href="http://www.ilbasilico.mx/" class="nk-portfolio-item-link" target="_blank">
+								<i class="fa fa-globe "></i>
+							</a>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="nk-isotope-item" data-filter="Social">
+			<div class="nk-portfolio-item nk-portfolio-item-info-style-2 nk-portfolio-item-sm">
+  
+				<div class="nk-portfolio-item-image">
+					<div style="background-image: url('<?php bloginfo('template_directory');?>/assets/img/logos/MARCAS-LOGOS800X800_ROADHOUSE.jpg')"></div>
+				</div>
+				<div class="nk-portfolio-item-info nk-portfolio-item-info-center text-center">
+					<div>
+						<h2 class="portfolio-item-title h3">Road House</h2>
+							<div class="portfolio-item-category">
+
+							<a href="https://www.facebook.com/RoadhouseSLP/" class="nk-portfolio-item-link" target="_blank">
+								<i class="fa fa-facebook pdleft-2-4"></i>
+							</a>
+
+							<a href="https://www.instagram.com/roadhouse_slp/" class="nk-portfolio-item-link" target="_blank">
+								<i class="fa fa-instagram pdleft-3-4"></i>
+							</a>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="nk-isotope-item" data-filter="Web Landing">
+			<div class="nk-portfolio-item nk-portfolio-item-info-style-2 nk-portfolio-item-sm">
+			   
+				<div class="nk-portfolio-item-image">
+					<div style="background-image: url('<?php bloginfo('template_directory');?>/assets/img/logos/MARCAS-LOGOS800X800_CONTAINERPARK.jpg')"></div>
+				</div>
+				<div class="nk-portfolio-item-info nk-portfolio-item-info-center text-center">
+					<div>
+						<h2 class="portfolio-item-title h3">Container Park</h2>
+						<div class="portfolio-item-category ">
+
+							<a href="http://containerparkslp.com/" class="nk-portfolio-item-link" target="_blank">
+								<i class="fa fa-globe "></i>
+							</a>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<div class="nk-isotope-item" data-filter="Social">
+			<div class="nk-portfolio-item nk-portfolio-item-info-style-2 nk-portfolio-item-sm">
+			   
+				<div class="nk-portfolio-item-image">
+					<div style="background-image: url('<?php bloginfo('template_directory');?>/assets/img/logos/MARCAS-LOGOS800X800_DENISSEMEZA.jpg')"></div>
+				</div>
+				<div class="nk-portfolio-item-info nk-portfolio-item-info-center text-center">
+					<div>
+						<h2 class="portfolio-item-title h3">Denisse Meza</h2>
+						 <div class="portfolio-item-category">
+
+							<a href="https://www.facebook.com/coachdenmeza/" class="nk-portfolio-item-link" target="_blank">
+								<i class="fa fa-facebook pdleft-2-4"></i>
+							</a>
+
+							<a href="https://www.instagram.com/coachdenmeza/" class="nk-portfolio-item-link" target="_blank">
+								<i class="fa fa-instagram pdleft-3-4"></i>
+							</a>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="nk-isotope-item" data-filter="Social">
+			<div class="nk-portfolio-item nk-portfolio-item-info-style-2 nk-portfolio-item-sm">
+			 
+				<div class="nk-portfolio-item-image">
+					<div style="background-image: url('<?php bloginfo('template_directory');?>/assets/img/logos/MARCAS-LOGOS800X800_RAULSBAR.jpg')"></div>
+				</div>
+				<div class="nk-portfolio-item-info nk-portfolio-item-info-center text-center">
+					<div>
+						<h2 class="portfolio-item-title h3">Raul´s Cantina Tradicional</h2>
+						 <div class="portfolio-item-category">
+
+							<a href="https://www.facebook.com/raulsbar/" class="nk-portfolio-item-link" target="_blank">
+								<i class="fa fa-facebook pdleft-2-4"></i>
+							</a>
+
+							<a href="https://www.instagram.com/raulsbarslp/" class="nk-portfolio-item-link" target="_blank">
+								<i class="fa fa-instagram pdleft-3-4"></i>
+							</a>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+
+		<div class="nk-isotope-item" data-filter="Social">
+			<div class="nk-portfolio-item nk-portfolio-item-info-style-2 nk-portfolio-item-sm">
+			
+				<div class="nk-portfolio-item-image">
+					<div style="background-image: url('<?php bloginfo('template_directory');?>/assets/img/logos/MARCAS-LOGOS800X800_YEIYARI.jpg')"></div>
+				</div>
+				<div class="nk-portfolio-item-info nk-portfolio-item-info-center text-center">
+					<div>
+						<h2 class="portfolio-item-title h3">Yeiyari</h2>
+							<div class="portfolio-item-category">
+
+							<a href="https://www.facebook.com/yeiyarinaturalbeauty/" class="nk-portfolio-item-link" target="_blank">
+								<i class="fa fa-facebook pdleft-1-3"></i>
+							</a>
+
+							<a href="https://www.instagram.com/yeiyari_/" class="nk-portfolio-item-link" target="_blank">
+								<i class="fa fa-instagram pdleft-2-3"></i>
+							</a>
+								
+								
+							<a href="port/portafolio-yeiyari.html" class="nk-portfolio-item-link">
+								<i class="fa fa-newspaper-o pdleft-3-3"></i>
+							</a>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+
+
+		<div class="nk-isotope-item" data-filter="Social">
+			<div class="nk-portfolio-item nk-portfolio-item-info-style-2 nk-portfolio-item-sm">
+			   
+				<div class="nk-portfolio-item-image">
+					<div style="background-image: url('<?php bloginfo('template_directory');?>/assets/img/logos/MARCAS-LOGOS800X800_NOMILIVE.jpg')"></div>
+				</div>
+				<div class="nk-portfolio-item-info nk-portfolio-item-info-center text-center">
+					<div>
+						<h2 class="portfolio-item-title h3">Nomilive</h2>
+						 <div class="portfolio-item-category">
+
+							<a href="https://www.facebook.com/nomilivegestionintegraldenominas/" class="nk-portfolio-item-link" target="_blank">
+								<i class="fa fa-facebook pdleft-1-3"></i>
+							</a>
+
+							<a href="https://www.linkedin.com/company/18770133" class="nk-portfolio-item-link" target="_blank">
+								<i class="fa fa-linkedin pdleft-2-3"></i>
+							</a>
+								
+							<a href="http://www.nomilive.com/" class="nk-portfolio-item-link" target="_blank">
+								<i class="fa fa-globe pdleft-3-3"></i>
+							</a>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="nk-isotope-item" data-filter="Social">
+			<div class="nk-portfolio-item nk-portfolio-item-info-style-2 nk-portfolio-item-sm">
+			  
+				<div class="nk-portfolio-item-image">
+					<div style="background-image: url('<?php bloginfo('template_directory');?>/assets/img/logos/MARCAS-LOGOS800X800_SONORAS.jpg')"></div>
+				</div>
+				<div class="nk-portfolio-item-info nk-portfolio-item-info-center text-center">
+					<div>
+						<h2 class="portfolio-item-title h3">Sonoras Meat</h2>
+						  <div class="portfolio-item-category ">
+
+							<a href="https://www.facebook.com/SonorasMeatSanLuis/" class="nk-portfolio-item-link" target="_blank">
+								<i class="fa fa-facebook "></i>
+							</a>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+	<div class="nk-gap-5"></div>
+</div>
+
+
+<div class="nk-pagination nk-pagination-center">
+	<a href="#"></a>
+</div>
+
+
+</main>
 
 <?php
-get_sidebar();
+
 get_footer();
