10fa4dd91389587cc983076013ca066f04a5f919
min11benja <redoxz811@gmail.com>
2019-03-14 11:42:13 -0600

    added the portfolio page for yeiyari

--- portafolio-yeiyari-page.php
+++ portafolio-yeiyari-page.php
@@ -16,26 +16,385 @@ Template Name: Port-Yeiyari
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<div class="container_styled_1">
+        <div class="container margin_60_35">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="text-dark text-center">DISEÑO GRÁFICO Y REDES SOCIALES
+                        PRODUCTOS <strong>YEIYARI</strong></h1><br>
+                </div>
+            </div>
+            <!-- End row -->
+        </div>
+    </div>
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+    <div class="container">
+        <div class="nk-portfolio-single">
 
-			get_template_part( 'template-parts/content', 'page' );
+            <div class="nk-gap-4 mb-2"></div>
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+            <div class="row vertical-gap">
+                <div class="col-lg-8">
+                    <h3 class="nk-portfolio-title">Como ayudamos a Yeiyari con su marca</h3>
+                    <div class="nk-portfolio-info">
+                        <div class="nk-portfolio-text">
 
-		endwhile; // End of the loop.
-		?>
+                            <p class="text-dark">Yeiyari es un proyecto de emprendimiento social de jabones artesanales elaborados por mujeres emprendedoras del Altiplano potosino.</p>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+                            <p class="text-dark">El desafío de crear una imagen minimalista que fuera innovadora en el giro de productos naturales de belleza y que mantenga la esencia de la marca fue aceptado por el equipo Ananá obteniendo toda una línea de diseño y comunicación.</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-lg-4">
+                    <table class="nk-portfolio-details">
+                        <tr>
+                            <td>
+                                <strong>Cliente:</strong>
+                            </td>
+                            <td>
+                                <a href="https://www.instagram.com/yeiyari_/" target="_blank">Yeiyari</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <strong>Fecha:</strong>
+                            </td>
+                            <td>02.05.2017</td>
+                        </tr>
+
+
+                        <tr>
+                            <td>
+                                <strong>
+                                    Servicio:
+                                </strong>
+                            </td>
+                            <td>
+                                Manejo de redes sociales
+                            </td>
+
+                        </tr>
+                        <tr>
+                            <td>
+                                <p class="text-dark"> <a href="https://www.facebook.com/yeiyarinaturalbeauty/" target="_blank"><i class="fa fa-facebook"></i></a>
+                                    <a href="https://www.instagram.com/yeiyari_/" target="_blank"><i class="fa fa-instagram"></i></a>
+
+                                </p>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            <div class="nk-gap-4 mnb-10"></div>
+
+        </div>
+    </div>
+
+
+    <!-- START: Features -->
+    <div class="nk-box bg-white">
+        <div class="nk-gap-5 mnt-6"></div>
+        <div class="container">
+
+            <div class="row vertical-gap md-gap">
+
+                <div class="col-md-6 col-lg-3">
+                    <div class="nk-ibox-3">
+
+                        <div class="nk-ibox-cont">
+                            <div class="nk-ibox-title">Calendario de contenidos</div>
+                            <div class="nk-ibox-text"></div>
+                        </div>
+                        <div class="nk-ibox-icon">
+                            <i class="fa fa-calendar" aria-hidden="true"></i>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3">
+                    <div class="nk-ibox-3">
+
+                        <div class="nk-ibox-cont">
+                            <div class="nk-ibox-title">Diseño grafico por post</div>
+                            <div class="nk-ibox-text"></div>
+                        </div>
+                        <div class="nk-ibox-icon">
+                            <i class="fa fa-image"></i>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3">
+                    <div class="nk-ibox-3">
+
+                        <div class="nk-ibox-cont">
+                            <div class="nk-ibox-title">Manejo de fb ads</div>
+                            <div class="nk-ibox-text"></div>
+                        </div>
+                        <div class="nk-ibox-icon">
+                            <i class="fa fa-magic" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3">
+                    <div class="nk-ibox-3">
+
+                        <div class="nk-ibox-cont">
+                            <div class="nk-ibox-title">Mensaje personalizado</div>
+                            <div class="nk-ibox-text"></div>
+                        </div>
+                        <div class="nk-ibox-icon">
+                            <i class="fa fa-newspaper-o"></i>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+        <div class="nk-gap-5 mnt-6"></div>
+    </div>
+    <!-- END: Features -->
+    <div class="nk-gap-5"></div>
+    <div class="nk-gap-5"></div>
+
+    <!-- START: Reviews -->
+    <div class="nk-box bg-main">
+        <div class="nk-gap-5 mnt-6"></div>
+        <div class="nk-gap-3"></div>
+        <div class="container">
+            <blockquote class="nk-blockquote-style-1 text-white">
+                <p class="text-dark">Hola! Mi experiencia con ANANA ha sido siempre excelente por su gran sensibilidad y apoyo en el area creativa en mi negocio.</p>
+                <cite class="text-dark">Lissette Leyequien - Fundadora de Yeiyari</cite>
+            </blockquote>
+        </div>
+        <div class="nk-gap-3"></div>
+        <div class="nk-gap-5 mnt-15"></div>
+    </div>
+    <!-- END: Reviews -->
+
+    <div class="nk-gap-5"></div>
+    <!--CARROUSEL-->
+
+    <div class="owl-nome owl-carousel owl-theme">
+        
+        <div class="item">
+            <img src="<?php bloginfo('template_directory');?>/assets/img/YEIYARI/JABONES-SCROLL/JABONES_200-300_CAFE.jpg" class="img-fluid" alt="Responsive image">
+        </div>
+        
+        <div class="item">
+            <img src="<?php bloginfo('template_directory');?>/assets/img/YEIYARI/JABONES-SCROLL/JABONES_200-300_CARBON.jpg" class="img-fluid" alt="Responsive image">
+        </div>
+        
+        <div class="item">
+            <img src="<?php bloginfo('template_directory');?>/assets/img/YEIYARI/JABONES-SCROLL/JABONES_200-300_LAVANDA.jpg" class="img-fluid" alt="Responsive image">
+        </div>
+
+        <div class="item">
+            <img src="<?php bloginfo('template_directory');?>/assets/img/YEIYARI/JABONES-SCROLL/JABONES_200-300_MENTA.jpg" class="img-fluid" alt="Responsive image">
+        </div>
+        
+        <div class="item">
+            <img src="<?php bloginfo('template_directory');?>/assets/img/YEIYARI/JABONES-SCROLL/JABONES_200-300_ROSAS.jpg" class="img-fluid" alt="Responsive image">
+        </div>
+        
+        <div class="item">
+            <img src="<?php bloginfo('template_directory');?>/assets/img/YEIYARI/JABONES-SCROLL/JABONES_200-300_YOGURT.jpg" class="img-fluid" alt="Responsive image">
+        </div>
+    
+    </div>
+
+    <!--CARROUSEL-->
+
+    <div class="nk-gap-5"></div>
+
+    <div id="portfolio-insta-yeiyari" class="container portfolio-grid-1">
+        <div class="nk-isotope nk-isotope-1-cols nk-isotope-gap nk-popup-gallery nk-portfolio-list">
+
+            <div class="nk-isotope-item" data-filter="Design">
+                <div class="nk-portfolio-item nk-portfolio-item-square nk-portfolio-item-info-style-1">
+
+                    <div class="nk-portfolio-item-image">
+                        <div style="background-image: url('<?php bloginfo('template_directory');?>/assets/img/YEIYARI/YEIYARI_REDES_INSTA.jpg');"></div>
+                    </div>
+                    <div class="nk-portfolio-item-info nk-portfolio-item-info-center text-center bg-main-9">
+                        <div>
+                            <div class="portfolio-item-icon">
+                                <i class="fa fa-eye"></i>
+                                <span class="overlay-img-text"> Instagram</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+    <div id="portfolio-catalogo-yeiyari" class="container portfolio-grid-1">
+        <div class="nk-isotope nk-isotope-1-cols nk-isotope-gap nk-popup-gallery nk-portfolio-list">
+
+            <div class="nk-isotope-item" data-filter="Design">
+                <div class="nk-portfolio-item nk-portfolio-item-square nk-portfolio-item-info-style-1">
+
+                    <div class="nk-portfolio-item-image">
+                        <div style="background-image: url('<?php bloginfo('template_directory');?>/assets/img/YEIYARI/YEIYARI_CATALOGO.jpg');"></div>
+                    </div>
+                    <div class="nk-portfolio-item-info nk-portfolio-item-info-center text-center bg-main-9">
+                        <div>
+                            <div class="portfolio-item-icon">
+                                <i class="fa fa-eye"></i>
+                                <span class="overlay-img-text"> Catalogo</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+    <div id="portfolio-grid-4" class="container">
+        <div class="nk-isotope  nk-isotope-gap nk-popup-gallery nk-portfolio-list">
+
+            <div class="nk-isotope-item" data-filter="Design">
+                <div class="nk-portfolio-item nk-portfolio-item-square nk-portfolio-item-info-style-1">
+
+                    <div class="nk-portfolio-item-image">
+                        <div style="background-image: url('<?php bloginfo('template_directory');?>/assets/img/YEIYARI/YEIYARI_PRODUCTO1.jpg');"></div>
+                    </div>
+                    <div class="nk-portfolio-item-info nk-portfolio-item-info-center text-center bg-main-9">
+                        <div>
+                            <div class="portfolio-item-icon">
+                                <i class="fa fa-eye"></i>
+                                <span class="overlay-img-text"> Shampoo</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="nk-isotope-item" data-filter="Design">
+                <div class="nk-portfolio-item nk-portfolio-item-square nk-portfolio-item-info-style-1">
+
+                    <div class="nk-portfolio-item-image">
+                        <div style="background-image: url('<?php bloginfo('template_directory');?>/assets/img/YEIYARI/YEIYARI_PRODUCTO2.jpg');"></div>
+                    </div>
+                    <div class="nk-portfolio-item-info nk-portfolio-item-info-center text-center bg-main-9">
+                        <div>
+                            <div class="portfolio-item-icon">
+                                <i class="fa fa-eye"></i>
+                                <span class="overlay-img-text"> Talco</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="nk-isotope-item" data-filter="Design">
+                <div class="nk-portfolio-item nk-portfolio-item-square nk-portfolio-item-info-style-1">
+
+                    <div class="nk-portfolio-item-image">
+                        <div style="background-image: url('<?php bloginfo('template_directory');?>/assets/img/YEIYARI/YEIYARI_PRODUCTO3.jpg');"></div>
+                    </div>
+                    <div class="nk-portfolio-item-info nk-portfolio-item-info-center text-center bg-main-9">
+                        <div>
+                            <div class="portfolio-item-icon">
+                                <i class="fa fa-eye"></i>
+                                <span class="overlay-img-text"> Jabon</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="nk-isotope-item" data-filter="Design">
+                <div class="nk-portfolio-item nk-portfolio-item-square nk-portfolio-item-info-style-1">
+
+                    <div class="nk-portfolio-item-image">
+                        <div style="background-image: url('<?php bloginfo('template_directory');?>/assets/img/YEIYARI/YEIYARI_PRODUCTO4.jpg');"></div>
+                    </div>
+                    <div class="nk-portfolio-item-info nk-portfolio-item-info-center text-center bg-main-9">
+                        <div>
+                            <div class="portfolio-item-icon">
+                                <i class="fa fa-eye"></i>
+                                <span class="overlay-img-text"> Regalo</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+    <div class="container portfolio-grid-1">
+        <div class="nk-isotope nk-isotope-1-cols nk-isotope-gap nk-popup-gallery nk-portfolio-list">
+
+            <div class="nk-isotope-item" data-filter="Design">
+                <div class="nk-portfolio-item nk-portfolio-item-square nk-portfolio-item-info-style-1">
+
+                    <div class="nk-portfolio-item-image">
+                        <div style="background-image: url('<?php bloginfo('template_directory');?>/assets/img/YEIYARI/YEIYARI_PAPELERIA.jpg');"></div>
+                    </div>
+                    <div class="nk-portfolio-item-info nk-portfolio-item-info-center text-center bg-main-9">
+                        <div>
+                            <div class="portfolio-item-icon">
+                                <i class="fa fa-eye"></i>
+                                <span class="overlay-img-text"> Branding</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+    <div class="nk-gap-5"></div>
+
+
+    <!-- START: Need a Project? -->
+    <div class="bg-black text-center">
+        <div class="nk-gap-5 mnb-12"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 offset-lg-2">
+                    <h2 class="text-white">¿Quieres algo similar?</h2>
+                    <div class="nk-gap-1 mnt-15"></div>
+                    <p class="text-white">Cuéntanos lo que estás buscando hacer o promover. Le damos un vistazo y vemos si esto podría ser el comienzo de algo hermoso para ti y tu negocio.</p>
+                    <div class="nk-gap mnt-9"></div>
+					<!--contact page link id=28 -->
+                    <a href="<?php echo get_page_link(28); ?>" class="btn btn-outline-black btn-outline-secondary mb5 btn-rounded">CONTÁCTANOS</a>
+                </div>
+            </div>
+        </div>
+        <div class="nk-gap-5"></div>
+    </div>
+    <!-- END: Need a Project? -->
+
+
+    <div class="nk-gap-5"></div>
+
+    <!-- START: Pagination -->
+    <div class="nk-pagination nk-pagination-center">
+        <div class="container">
+		<!--contact page link id=28 -->
+            <a class="nk-pagination-prev" href="<?php echo get_page_link(71); ?>">
+                <span class="fa fa-angle-left"></span> Proyecto previo</a>
+			<!--portfolio cat page link id=20 -->
+            <a class="nk-pagination-center" href="<?php echo get_page_link(20); ?>">
+                <span class="nk-icon-squares"></span>
+            </a>
+            <a class="nk-pagination-next" href="#"> </a>
+        </div>
+    </div>
+    <!-- END: Pagination -->
 
 <?php
-get_sidebar();
+
 get_footer();
